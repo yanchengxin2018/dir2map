1e42aaf8a9dddddac754712716d3d34e02bd6760
闫成欣 <yan.cheng.xin@foxmail.com>
2025-03-26 07:50:43 +0800

    2.36

--- src/dirtree/main.py
+++ src/dirtree/main.py
@@ -1,58 +1,100 @@
 import os
 from pathlib import Path
+import argparse
+
+base_root_path = None
 
 
-def print_directory_structure(root_dir, prefix="", exclude_dirs=None):
+def _ignore_dir(dir_path: Path, exclude_dirs: list, ):
+    if not dir_path.is_dir():
+        return False
+
+    for exclude_dir in exclude_dirs:
+        exclude_dir_path = base_root_path / exclude_dir
+        try:
+            dir_path.relative_to(exclude_dir_path)
+            return True
+        except ValueError:
+            continue
+    return False
+
+
+def get_directory_structure(root_dir, prefix="", exclude_dirs=None):
     """
-    递归生成目录结构的每行内容列表
     :param root_dir:
     :param prefix:
     :param exclude_dirs:
     :return:
     """
+    global base_root_path
+    if not base_root_path:
+        base_root_path = Path(root_dir)
     result = []
     if exclude_dirs is None:
         exclude_dirs = []
+
     try:
-        # 获取当前目录下的所有文件和文件夹
         items = os.listdir(root_dir)
     except PermissionError:
-        result.append(f"没有权限访问 {root_dir}，跳过该目录。")
+        result.append(f"There is no permission to access {root_dir}, so this directory will be skipped.")
         return result
+
     for index, item in enumerate(items):
-        # 拼接当前项的完整路径
         item_path = os.path.join(root_dir, item)
-        # 判断是否为需要排除的目录
-        if os.path.isdir(item_path) and item in exclude_dirs:
+
+        if _ignore_dir(Path(str(item_path)), exclude_dirs, ):
             continue
-        # 判断是否为最后一个项
+
         is_last = index == len(items) - 1
-        # 根据是否为最后一个项选择不同的前缀符号
         connector = "└── " if is_last else "├── "
-        # 生成当前项的显示内容
         line = prefix + connector + item
         result.append(line)
-        # 如果当前项是文件夹，则递归调用函数
         if os.path.isdir(item_path):
-            # 根据是否为最后一个项选择不同的子前缀
             new_prefix = prefix + ("    " if is_last else "│   ")
-            sub_result = print_directory_structure(item_path, new_prefix, exclude_dirs)
+            sub_result = get_directory_structure(item_path, new_prefix, exclude_dirs)
             result.extend(sub_result)
     return result
 
 
+def get_params():
+    parser = argparse.ArgumentParser(description='获取目录相关参数')
+    parser.add_argument('-r', '--root_dir', type=Path, required=True,
+                        help='根目录路径')
+    parser.add_argument('-e', '--exclude_dirs', type=str, default='',
+                        help='要排除的目录，多个目录用逗号分隔')
+    parser.add_argument('-o', '--out_path', type=Path, default=None, required=False,
+                        help='把结果保存到此文件')
+    args = parser.parse_args()
+
+    root_dir = args.root_dir
+    out_path = args.out_path
+    if args.exclude_dirs:
+        exclude_dirs = args.exclude_dirs.split(',')
+    else:
+        exclude_dirs = []
+
+    return root_dir, exclude_dirs, out_path
+
+
 def main():
-    root_dir = r'D:\A\assistant\src\server'
-    # root_dir = r'.'
-    result = print_directory_structure(root_dir=root_dir, exclude_dirs=[
-        '.git',
-        '.next',
-        '.idea',
-        'node_modules',
-    ])
+    params = get_params()
+    root_dir, exclude_dirs, out_path = params
+    # root_dir = r'D:\A\dirtree\src'
+    # exclude_dirs = [
+    #     '.git',
+    #     '.next',
+    #     '.idea',
+    #     'node_modules',
+    #     # 'dirtree/t1/t2',  # As an example
+    # ]
+    result = get_directory_structure(root_dir=root_dir, exclude_dirs=exclude_dirs, )
     result = [Path(root_dir).absolute().name] + result
     result = '\n'.join(result)
     print(result)
+    if out_path:
+        Path(out_path).parent.mkdir(parents=True, exist_ok=True)
+        with open(str(out_path), 'w', encoding='utf8', ) as file_obj:
+            file_obj.write(result)
 
 
 main()
